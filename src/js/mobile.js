1563050753f0271fc0adb6c8b8a2c13a02418fff
Romullo Sander <romullo_sas@outlook.com>
2018-12-20 02:58:04 -0200

    desktop

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
 
-
+    // abre a janela + filtrar
     $('#filt').on('click', function () {
 
         $('#opcoes').css('left', '0%');
         $('#filtrar').css('left', '0%');
     });
 
+
+    // abre a janela + ordenar
     $('#ord').on('click', function () {
 
         $('#opcoes').css('left', '0%');
         $('#ordenar').css('left', '0%');
     });
 
+
+    // fecha a janela + ordenar e filtrar
     $('.fcjs').on('click', function () {
 
         $('#opcoes').css('left', '-100%');
@@ -20,6 +24,10 @@ $(document).ready(function () {
         $('#filtrar').css('left', '-100%');
     });
 
+    // flag para abrir botões ( aplicar + limpar)
+    var size2 = 0;
+
+    // abre filtros
     $('.fcjs2').on('click', function () {
 
         var id = $(this).attr('id');
@@ -30,19 +38,22 @@ $(document).ready(function () {
         switch (id) {
             case 'fc1':
                 {
-                    size = '1190px';
+                    size = '650px';
+                    // $('#optbut').css('height', '46px');
                     break;
                 }
 
             case 'fc2':
                 {
-                    size = '408px';
+                    size = '180px';
+                    //  $('#optbut').css('height', '46px');
                     break;
                 }
 
             case 'fc3':
                 {
-                    size = '595px';
+                    size = '292px';
+                    // $('#optbut').css('height', '46px');
                     break;
                 }
         }
@@ -51,9 +62,31 @@ $(document).ready(function () {
 
         if (tam != '0px') {
             size = '0px';
+            size2 -= 1;
+        } else {
+            size2 += 1;
         }
 
         $('.' + id).css('height', size);
+
+
+        // botões aplicar + limpar
+        if (size2 == 0) {
+            $('#optbut').css('height', '0px');
+        }
+
+        if (size2 > 0) {
+            $('#optbut').css('height', '46px');
+        }
+
+        // correção possivel erro
+        if (size2 < 0) {
+            size2 = 0;
+        }
+
+        if (size2 > 3) {
+            size2 = 3;
+        }
 
     });
 });
