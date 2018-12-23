24b95dc86f39ee50adbce60b9d71d53da24898e9
Heraldo Ecard Roque Filho <heraldo663@gmail.com>
2018-12-23 13:16:49 -0200

    order by mobile menu box is working

--- src/js/modules/Dropdown.js
+++ src/js/modules/Dropdown.js
@@ -6,17 +6,36 @@ export default class Dropdown {
     selectors() {
         this.dropDownColor = document.getElementById('dropdown-colors');
         this.colorForm = document.getElementById("color-desktop");
+        this.mobileMenu = document.getElementById("mobile-menu");
+        this.mobileMenuOrderBy = document.querySelector(".mobile-menu__order-by");
+        this.mobileOrderByMenuBtn = document.getElementById('order-by-mobile-button');
+        this.btnXOrderBy = document.getElementById('btn-x-order-by');
     }
     eventListeners() {
-        this.dropDownColor.addEventListener('click', this.toggleMenu.bind(this));
+        this.dropDownColor.addEventListener('click', this.toggleColorsMenu.bind(this));
+        this.mobileOrderByMenuBtn.addEventListener('click', this.toggleMobileOrderByMenu.bind(this));
+        this.btnXOrderBy.addEventListener('click', this.closeOrderByMobileFilter.bind(this));
     }
-    toggleMenu(e) {
+    toggleColorsMenu(e) {
         e.preventDefault();
         if (!this.colorForm.classList.value.includes('show-colors')) {
             this.colorForm.classList.add('show-colors');
         } else {
             this.colorForm.classList.remove('show-colors');
         }
+    }
+    toggleMobileOrderByMenu() {
+        if (
+            !this.mobileMenu.classList.value.includes('show') &&
+            !this.mobileMenuOrderBy.classList.value.includes('show')
+        ) {
+            this.mobileMenu.classList.add('show');
+            this.mobileMenuOrderBy.classList.add('show');
+        }
+    }
+    closeOrderByMobileFilter() {
+        this.mobileMenu.classList.remove('show');
+        this.mobileMenuOrderBy.classList.remove('show');
     }
 
 }
