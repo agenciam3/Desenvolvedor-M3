03d65d6ea233e2b9199eea8564b0fa5f02219977
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-08-13 12:00:38 -0300

    initial commits

---
+++ testem3/src/App.js
@@ -0,0 +1,12 @@
+import Products from "./screens/Products";
+import './styles.css'
+
+function App() {
+  return (
+    <div className="main-container">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
