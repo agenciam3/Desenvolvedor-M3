92d0192a59b8d979c328382ac07872b7222146b6
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-08-13 15:49:09 -0300

    adicionando imagem ao card do produto

--- testem3/src/screens/Products/index.jsx
+++ testem3/src/screens/Products/index.jsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from 'react';
 import './styles.css'
 import ProductCard from '../../components/ProductCard';
+import ProductsFilters from '../../components/ProductsFilters';
+import img2 from '../../imagens/img_2.png'
+import img3 from '../../imagens/img_3.png'
+import img4 from '../../imagens/img_4.png'
+import img5 from '../../imagens/img_5.png'
+import img6 from '../../imagens/img_6.png'
+import img7 from '../../imagens/img_7.png'
+import img8 from '../../imagens/img_8.png'
+import img9 from '../../imagens/img_9.png'
+import img10 from '../../imagens/img_10.png'
+
 
 const list = [
     {
@@ -8,70 +19,78 @@ const list = [
         name: "Camiseta Mescla",
         price: "28,00",
         color: "Cinza",
-        installment: 3
+        installment: 3,
+        img: img2
     },
     {
         id: 2,
         name: "Saia em couro",
         price: "398,00",
         color: "Azul",
-        installment: 5
+        installment: 5,
+        img: img3
     },
     {
         id: 3,
         name: "Cardigan tigre",
         price: "28,00",
         color: "Amarelo",
-        installment: 5
+        installment: 5,
+        img: img4
     },
     {
         id: 4,
         name: "Cardigan off white",
         price: "99,00",
         color: "Branco",
-        installment: 3
+        installment: 3,
+        img: img5
     },
     {
         id: 5,
         name: "Body leopardo",
         price: "129,00",
         color: "Amarelo",
-        installment: 3
+        installment: 3,
+        img: img6
     },
     {
         id: 6,
         name: "Casaco Pelos",
         price: "28,00",
         color: "Rosa",
-        installment: 5
+        installment: 5,
+        img: img7
     },
     {
         id: 7,
         name: "Cropped Stripes",
         price: "120,00",
         color: "Laranja",
-        installment: 3
+        installment: 3,
+        img: img8
     },
     {
         id: 8,
         name: "Camisa Transparente",
         price: "398,00",
         color: "Preto",
-        installment: 5
+        installment: 5,
+        img: img9
     },
     {
         id: 9,
         name: "Pochete clutch",
         price: "99,00",
         color: "Preto",
-        installment: 3
+        installment: 3,
+        img: img10
     }
 ]
 
 const Products = () => {
     const [colors, setColors] = useState([])
     const [allColors, setAllColors] = useState([])
-    const [showAllColors, setShowAllColors] = useState(false)
     useEffect(() => {
         let cont = 1
         let colorsA = []
@@ -85,64 +104,15 @@ const Products = () => {
         colorsA.map((newColor) => { setAllColors(oldColors => [...oldColors, newColor]) })
 
     }, [])
-
-    const sizes = ["P", "M", "G", "GG", "U", "36", "38", "40", "42", "44"]
     return (
         <div className="container-products">
             <div className="filters">
-                <div className="color-filter">
-                    <label>Cores</label>
-                    {showAllColors ? allColors.map((item, index) => {
-                        return (
-                            <div key={index}>
-                                <input type="checkbox" />
-                                <span>{item}</span>
-                            </div>
-                        )
-                    }) : colors.map((item, index) => {
-                        return (
-                            <div key={index}>
-                                <input type="checkbox" />
-                                <span>{item}</span>
-                            </div>
-                        )
-                    })}
-                    {showAllColors ? <button onClick={() => setShowAllColors(oldBool => !oldBool)}>
-                        Mostrar Menos
-                    </button> : <button onClick={() => setShowAllColors(oldBool => !oldBool)}>
-                        Mostrar Mais
-                    </button>}
-                </div>
-                <div className="sizes">
-                    <label>Tamanhos</label>
-                    <div className="btn-size-area">
-                        {sizes.map((item, index) => {
-                            return (
-                                <button key={index} className="btn-size">
-                                    {item}
-                                </button>
-                            )
-                        })}
-                    </div>
-                </div>
-                <div className="prices">
-                    <label>Faixa de Preço</label>
-                    <input type="checkbox" />
-                    <span>de R$ 0 até R$50</span>
-                    <input type="checkbox" />
-                    <span>de R$ 51 até R$150</span>
-                    <input type="checkbox" />
-                    <span>de R$ 151 até R$300</span>
-                    <input type="checkbox" />
-                    <span>de R$ 301 até R$500</span>
-                    <input type="checkbox" />
-                    <span>a partir de R$501</span>
-                </div>
+                <ProductsFilters allColors={allColors} colors={colors} />
             </div>
             <div className="cards-area">
                 {list.map((item) => {
                     return (
-                        <ProductCard item={item} />
+                        <ProductCard item={item} list={list} />
                     )
                 })}
             </div>
