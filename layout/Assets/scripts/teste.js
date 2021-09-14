755bc03044aa8006e924950438544ebe4a894a73
Murilo <murilofer@yahoo.com.br>
2021-09-14 16:31:26 -0300

    Feature/master2

--- layout/Assets/scripts/teste.js
+++ layout/Assets/scripts/teste.js
@@ -1,22 +1,30 @@
+fetch("dados.json")
+    .then(data =>{
+        console.log(data)
+        
+    })
 
 
 
-let estoqueM3 = 
-[
-    {id:1, produto:'CAMISA MESCLA', informacoes:{ preco: 28.00, cor: 'Cinza' } }, 
-    {id:2, produto:'SAIA EM COURO', informacoes:{ preco: 398.00, cor: 'Laranja'} },
-    {id:3, produto:'CARDIGAN TIGRE', informacoes:{ preco: 398.00, cor: 'Laranja'} },
-    {id:4, produto:'CARDIGAN OFF WHITE', informacoes:{ preco: 99.00, cor: 'Branco'} },
-    {id:5, produto:'BODY LEOPARDO', informacoes:{ preco: 129.00, cor: 'Amarelo'} },
-    {id:6, produto:'CASACO PELOS', informacoes:{ preco: 398.00, cor: 'Rosa'} },
-    {id:7, produto:'CROPPED STRIPES', informacoes:{ preco: 120.00, cor: 'Vermelho'} },
-    {id:8, produto:'CAMISA TRANSPARENT', informacoes:{ preco: 398.00, cor: 'Preto'} },
-    {id:9, produto:'POCHETE CLUTCH',informacoes:{ preco: 99.00, cor: 'Branco'} },
-]
 
-estoqueM3.forEach((item, index) => {
-    console.log(item, index)
-})
+
+
+// let estoqueM3 = 
+// [
+//     {id:1, produto:'CAMISA MESCLA', informacoes:{ preco: 28.00, cor: 'Cinza' } }, 
+//     {id:2, produto:'SAIA EM COURO', informacoes:{ preco: 398.00, cor: 'Laranja'} },
+//     {id:3, produto:'CARDIGAN TIGRE', informacoes:{ preco: 398.00, cor: 'Laranja'} },
+//     {id:4, produto:'CARDIGAN OFF WHITE', informacoes:{ preco: 99.00, cor: 'Branco'} },
+//     {id:5, produto:'BODY LEOPARDO', informacoes:{ preco: 129.00, cor: 'Amarelo'} },
+//     {id:6, produto:'CASACO PELOS', informacoes:{ preco: 398.00, cor: 'Rosa'} },
+//     {id:7, produto:'CROPPED STRIPES', informacoes:{ preco: 120.00, cor: 'Vermelho'} },
+//     {id:8, produto:'CAMISA TRANSPARENT', informacoes:{ preco: 398.00, cor: 'Preto'} },
+//     {id:9, produto:'POCHETE CLUTCH',informacoes:{ preco: 99.00, cor: 'Branco'} },
+// ]
+
+// estoqueM3.forEach((item, index) => {
+//     console.log(item, index)
+// })
 
 
 // console.log(estoqueM3.map((item, index) => {
