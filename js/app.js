d093bd83db35da53dedfaf0828c279f9bad46514
Dayoripa <daulinri@gmail.com>
2021-11-25 00:49:06 -0500

    Se genera el menu mobile

--- js/app.js
+++ js/app.js
@@ -6,7 +6,17 @@ const templateCard = document.querySelector('#template__card').content;
 const templateItems= document.querySelector('#template-items').content;
 const fragment = document.createDocumentFragment();
 const car = document.querySelector('#car');
-let cart = {};
+let data = [];  // Save information of Json
+let cart = {}; // shopping cart
+
+// Selector for search
+const checks = document.querySelectorAll('.colors');
+const sizes = document.querySelectorAll('.sizes__item span');
+const checkPrice = document.querySelectorAll('.check__price');
+
+// Variable for Modal
+const modal = document.querySelector('#modal');
+
 
 // EventListener
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,22 +36,85 @@ emptyCart.addEventListener('click', () =>{
     limpiarCart();
 });
 
+ // EventListener for search checkboxes
+
+ for(let checkbox of checks){
+    checkbox.addEventListener('click', function(){
+        if(this.checked == true) {
+            searchData.cor = this.value
+            filterProduct();
+        }else{
+            paintCards(data)
+        }
+    })
+}
+
+// EventListener for search Sizes
+sizes.forEach(items =>{
+    items.addEventListener('click', function(){
+            items.classList.toggle('sizes__new');
+            if(items.classList.contains('sizes__new')){
+                searchData.tamanho = items.childNodes[0].data;
+                filterProduct()
+            }
+            else
+            paintCards(data)
+    })
+    
+});
+
+// EventListener for search prices
+for(let check of checkPrice){
+    check.addEventListener('click', function(){
+        if(this.checked == true) {
+            searchData.preco = parseInt(this.value)            
+            filterProduct()
+        }else{
+            paintCards(data)
+        }
+    })
+}
+
+//EvenListener for menu accordion
+const accordion = document.querySelectorAll('.accordion__content');
+
+for(i = 0; i<accordion.length; i++){
+    accordion[i].addEventListener('click', function(e){
+        if(e.target.classList.contains('accordion__label')){
+            this.classList.toggle('active')
+        }
+    })
+}
 
 
 // Funciones
+
+// function for open Modal
+const openModal = () =>{
+    modal.style.display = 'flex';
+}
+
+const closeModal = () =>{
+    modal.style.display = 'none';
+}
+
 const obtenerProductos = async() =>{
     try {
         const res = await fetch('datos.json');
         const dates = await res.json();
-        paintCards(dates)
+        data = dates;
+        paintCards(data)
     } catch (error) {
         console.log(error)
     }
 }
 
-const paintCards = dates =>{
-    dates.forEach(products => {
+// Paint cards
+const paintCards = data =>{
 
+    cleanHTML()
+    console.log(data)
+    data.forEach(products => {
         const {id, image, description, product, price} = products;
 
         templateCard.querySelector('.card__image').setAttribute('src', image);
@@ -56,6 +129,7 @@ const paintCards = dates =>{
     cards.appendChild(fragment)
 }
 
+// Add product to shopping cart
 const addProductsCar = e =>{
     e.preventDefault();
     const classBtn = e.target.classList.contains('card__button');
@@ -66,8 +140,9 @@ const addProductsCar = e =>{
     e.stopPropagation();
 }
 
+// Create an object with information of product
 const readDatesProduct = product =>{
-    // creo un objeto con la informacion del producto
+    
     objProduct = {
         image: product.querySelector('.card__image').src,
         id: product.querySelector('.card__button').dataset.id,
@@ -84,6 +159,7 @@ const readDatesProduct = product =>{
     paintShoppingCart();
 }
 
+// Paint information in the shopping cart
 const paintShoppingCart = () =>{
 
     limpiarCart();
@@ -103,13 +179,14 @@ const paintShoppingCart = () =>{
     })
     items.appendChild(fragment)
 }
-
+// Clean HTML shopping cart
 const limpiarCart = () =>{
     while(items.firstChild){
         items.removeChild(items.firstChild);
     }
 }
 
+// Delete an item of shopping cart
 const deletedCart = (e) =>{
     e.preventDefault();
 
@@ -124,4 +201,60 @@ const deletedCart = (e) =>{
     e.stopPropagation();
     
 }
+
+// Create object to filter products
+
+const searchData = {
+    cor: '',
+    tamanho: '',
+    preco: ''
+}
+
+function filterProduct(){
+    const result = data.filter(filtrarColor).filter(searchSizes).filter(searchPrice);
+    paintCards(result);
+}
+
+function filtrarColor(data){
+    const {cor} = searchData;
+    if(cor){
+        return data.color === cor;
+    }
+    return data;
+}
+
+const searchSizes = (data) => {
+    const {tamanho} = searchData;
+    if(tamanho){
+        return data.size === tamanho.toLowerCase();
+    }
+    return data;
+}
+
+const searchPrice = (data) =>{
+    const {preco} = searchData;
+    if(preco){
+        const valor = preco;
+            if(valor > 0 && valor <= 50){
+            return data.price > 0 && data.price <= 50;
+            }else 
+            if(valor > 50 && valor <= 150){
+                return data.price > 50 && data.price <= 150;
+            }else if(valor > 150 && valor <= 300){
+                return data.price > 150 && data.price <= 300;
+            }else if(valor > 300 && valor <= 500){
+                return data.price > 300 && data.price <= 500;
+            }else if(valor > 0 ){
+                return data;
+            }
+    }else
+        return data;
+}
+
+function cleanHTML(){
+    while(cards.firstChild){
+        cards.removeChild(cards.firstChild)
+    }
+}
+
 
