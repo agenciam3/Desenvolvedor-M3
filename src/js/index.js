df49756cc82675929026057ec922ae4f3b644a5f
Cauã <cauaproducoes@gmail.com>
2022-01-09 18:25:27 -0300

    moving work-station

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log("Dev m3");
+//babel bug fix
+import("babel-core/register");
+import("babel-polyfill");
+
+
+let products = [];self
+let url = "http://localhost:5500/products";
+async function getProducts(url){ 
+    let response = await fetch(url);
+    let data = await response.json();
+    
+    data.forEach(element => {
+        products.push(element); 
+    }); 
+    return products;
+}
+
+(async function () {
+    let promise = await getProducts(url);
+})
