55129c87be8673a8a278dfa4f95524b999a55c14
Cauã <cauaproducoes@gmail.com>
2022-01-14 02:04:07 -0300

    fully working filters

--- src/js/index.js
+++ src/js/index.js
@@ -5,19 +5,7 @@ function processProducts(data) {
         init: function () {
             product.bindEvent();
             //product.render(this.products);
-            let testa = [];
-            let testo = [];
-            let test = "de R$0 até R$50";
-            index = test.lastIndexOf("$");
-            testo.push(parseInt(test.substring(0, index).replace(/[^0-9]/g,'')));
-            testo.push(parseInt(test.substring(index).replace(/[^0-9]/g,'')));
-
-            testa.push(testo);
-            console.log(testo);
-
-
-
-
+            
         },
 
         render: function (products, found=true) {
@@ -29,50 +17,86 @@ function processProducts(data) {
         },
 
         filter: function (user_input) {
-            //filtra os elementos
+            // inicializa/reseta a temporary products
             let tmp_products = [];
+            //varre todos os elementos
+            this.products.forEach(element => {
+                //inicializa os fatores de filtro
+                let size = false;
+                let color = false;
+                let price = false;
 
-                    this.products.forEach(element => {
-                        let size = false;
-                        let color = false;
-                        let price = false;
-                        if (user_input.size.length !== 0) {
-                            element.size.forEach(product_size => {
-                                user_input.size.forEach(key => {
-                                    if (product_size == key) {
-                                        size = true;
-                                    }
-                                });
-                            });  
-                        }else size = true;
+                //size
+                if (user_input.size.length !== 0) {
+                    element.size.forEach(product_size => {
+                        user_input.size.forEach(key => {
+                            if (product_size == key) {
+                                size = true;
+                            }
+                        });
+                    });  
+                }else size = true;
 
-                        if (user_input.color.length !== 0) {
-                            user_input.color.forEach(key => {
-                                if (element.color == key) {
-                                    color = true;
-                                }
-                            });
-                        } else color = true;
-                    
-                    
-                        if (size && color) 
-                        tmp_products.push(element);
-                        
-                });
-        
-
-
+                //color
+                if (user_input.color.length !== 0) {
+                    user_input.color.forEach(key => {
+                        if (element.color == key) {
+                            color = true;
+                        }
+                    });
+                } else color = true;
+                
+                //price_range
+                if (!isNaN(user_input.price_range[0])) {
+                    if (element.price>= user_input.price_range[0] && element.price < user_input.price_range[1]) {
+                        console.log("is");
+                        price = true;
+                    }
+                } else price = true;
+                
+                //verifica se o produto atende os requisitos
+                if (size && color && price) 
+                    tmp_products.push(element);
+                
+            });
+            //caso o temporary esteja vazio verifica se pelo menos um filtro foi ativado
+            //caso verdadeiro significa que nenhum produto foi encontrado
             if (!tmp_products.length == 0) {
                 this.render(tmp_products);
-            } else if (user_input.size.length !== 0 || user_input.color.length !== 0 || user_input.price.length !== 0) {
+            } else if (user_input.size.length !== 0 || user_input.color.length !== 0 || !isNaN(user_input.price_range[0])) {
                 this.render(this.products, false)
             }
 
         },
  
         bindEvent: function () {
+            function setLastChecked () {
+                //set last checked for further loop become automatic
+                return {0: {nextSibling: {textContent: ""}}};
+            }
+            let last_select_checked = setLastChecked();
+            
+            function uncheck () {
+            var current_checked = Array.prototype.slice.call(document.getElementsByName("price_range")).filter(function(element){
+                return element.checked;
+            });
+            if (current_checked[0].nextSibling.textContent === last_select_checked[0].nextSibling.textContent) {
+                current_checked[0].checked = false;
+                last_select_checked = setLastChecked();
+            } else {
+                last_select_checked = current_checked;
+            }
+
+            }
+
             document.getElementById("wrapper").onclick = (function(event) {
-                if(event.target.type == 'checkbox'){
+                //uncheck select if selected
+                if (event.target.type == 'radio') {
+                    uncheck();
+                }
+
+                //only run for child elements
+                if(event.target.type == 'checkbox' || event.target.type == 'radio'){
                     let filter_data = document.getElementsByClassName("filters");
                     product.processUserInput(filter_data);
                   }
@@ -80,7 +104,6 @@ function processProducts(data) {
         },
 
         processUserInput: function (filter_data) {
-            console.log(filter_data);
             let user_input = {
                 color: [],
                 size: [],
@@ -94,7 +117,14 @@ function processProducts(data) {
                     user_input[option].push(item.nextSibling.textContent.trim());
                 }
             }
-            //console.log(user_input);
+
+            let tmp_price_array = [];
+            let tmp_price = user_input.price_range.toString();
+            index = tmp_price.lastIndexOf("$");
+            tmp_price_array.push(parseInt(tmp_price.substring(index, 0).replace(/[^0-9]/g,'')));
+            tmp_price_array.push(parseInt(tmp_price.substring(index).replace(/[^0-9]/g,'')));
+            user_input.price_range = tmp_price_array;
+
             this.filter(user_input);
         },
     }
