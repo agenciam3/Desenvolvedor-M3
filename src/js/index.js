210cf17674ccacac41e05336083db85ce97ad260
Cauã <cauaproducoes@gmail.com>
2022-01-17 23:47:52 -0300

    add to cart

--- src/js/index.js
+++ src/js/index.js
@@ -9,27 +9,34 @@ function processProducts(data) {
             this.tmp_products = this.products;
             this.load();            
         },
-
+        
         render: function (products, found) {
             // create and display elements | get the array of elements
-            console.log(products);
-
+            let products_div = document.getElementById("products");
+            
+            products.forEach(element => {
+                //console.log(element);
+                new_product = document.createElement("div");
+                new_product.innerHTML = "<div class='product'> <img src='"+ element.image +"' class='image'> <p class='name'>"+ element.new +"</p> <p class='price'>R$ "+ element.price.toFixed(2) +"</p> <p class= 'parcelamento'>Até "+ element.parcelamento[0] +"x de R$"+ element.parcelamento[1] +"</p> <button class='comprar' value='"+ element.id +"'>Comprar</button>";
+                products_div.appendChild(new_product);
+            });
+            
             if (!found) {
                 console.log("Can't find any products with current selection");
             }
         },
-
+        
         filter: function (user_input) {
             // initialize/reset temporary products
             let tmp_products = [];
-
+            
             //varre todos os elementos
             this.products.forEach(element => {
                 //inicializa os fatores de filtro
                 let size = false;
                 let color = false;
                 let price = false;
-
+                
                 //size
                 if (user_input.size.length !== 0) {
                     element.size.forEach(product_size => {
@@ -40,7 +47,7 @@ function processProducts(data) {
                         });
                     });  
                 }else size = true;
-
+                
                 //color
                 if (user_input.color.length !== 0) {
                     user_input.color.forEach(key => {
@@ -59,36 +66,37 @@ function processProducts(data) {
                 
                 // checks if the product meets the requirements
                 if (size && color && price) 
-                    tmp_products.push(element);
+                tmp_products.push(element);
                 
             });
-
+            
             if (order = user_input.order_by.length !== 0) {
                 switch (user_input.order_by) {
+
                     case "Menor Preço":
                         tmp_products.sort((element1, element2) => {
                             return element1.price - element2.price; 
                         });
                         break;
-                    
+                        
                     case "Maior Preço":
                         tmp_products.sort((element1, element2) => {
                             return element2.price - element1.price; 
                         });
                         break;
-                    
+                        
                     case "Mais Recentes":
                         tmp_products.sort((element1, element2) => {
                             return new Date(element2.date) - new Date(element1.date); 
                         });
                         break;
-                
-                    //caso o usuário tente interferir nas opções
+                            
+                                //caso o usuário tente interferir nas opções
                     default:
                         break;
-                }
+                    }
             }
-
+                            
             // if the temporary is empty check if at least one filter has been activated
             // true case means no product was found
             if (tmp_products.length !== 0) {
@@ -97,11 +105,11 @@ function processProducts(data) {
             } else if (user_input.size.length !== 0 || user_input.color.length !== 0 || !isNaN(user_input.price_range[0])) {
                 this.load(false)
             }
-
+            
         },
- 
+        
         bindEvent: function () {
-
+            
             // allows the radio to be deselected
             function setLastChecked () {
                 //set last checked for further loop become automatic
@@ -125,7 +133,7 @@ function processProducts(data) {
             selected.onclick = (function() {
                 options_container.classList.toggle("active");
             });
-
+            
             // close the order select and assign selected value
             let order = document.getElementById("selected");                 
             let options_container = document.getElementById("options_container");
@@ -137,14 +145,14 @@ function processProducts(data) {
                     element.firstChild.nextSibling.innerHTML = swap_text;
                     options_container.classList.remove("active");
                 });
-            
+                
             }
             
             
             // properly the event that captures de user input 
             document.getElementById("container").onclick = (function(event) { 
                 //only run for child elements
-                if(event.target.type === 'checkbox' || event.path.filter(element => element.id === "order_by_select").length !== 0 && event.path.filter(element => element.id === "options_container").length !== 0 || event.target.type === 'radio' ) { //(event.path.filter(element => element.id === "order_by_select") !== [] && event.path.filter(element => element.id === "options_container") !== [] )||
+                if(event.target.type === 'checkbox' || event.path.filter(element => element.id === "order_by_select").length !== 0 && event.path.filter(element => element.id === "options_container").length !== 0 || event.target.type === 'radio' ) {
                     //uncheck select if selected
                     if (event.target.name == "price_range") {
                         uncheck();
@@ -163,17 +171,17 @@ function processProducts(data) {
                     product.processUserInput(filter_data, order);
                 }
             })
-
+            
             load_more_btn = document.getElementById("load_more")
             load_more_btn.onclick = (function() {
-                 if(product.load()) {
+                if(product.load()) {
                     load_more_btn.textContent = "fim da lista";
                     load_more_btn.disabled = true;
-                 }
+                }
             });
             
         },
-
+        
         processUserInput: function (filter_data, order) {
             let user_input = {
                 color: [],
@@ -189,9 +197,9 @@ function processProducts(data) {
                 }                
             }            
             user_input.order_by = order.textContent.trim();
-
-
-
+            
+            
+            
             let tmp_price_array = [];
             let tmp_price = user_input.price_range.toString();
             index = tmp_price.lastIndexOf("$");
@@ -200,7 +208,7 @@ function processProducts(data) {
             user_input.price_range = tmp_price_array;
             this.filter(user_input);
         },
-
+        
         load: function (found=true) {
             if (found) {
                 var products = this.tmp_products;
@@ -211,40 +219,68 @@ function processProducts(data) {
             let sliced = undefined;
             let   device = window.innerWidth;
             switch (true) {
+
                 case (device>=320 && device<600) :
                     sliced = products.slice(current_shown_items,(current_shown_items+4));
                     this.current_shown_items += sliced.length;
                     break;
+
                 case (device>=600) :
                     sliced = products.slice(current_shown_items,(current_shown_items+9));
                     this.current_shown_items += sliced.length;
                     break;
+
                 default:
                     break;
+
             }
             this.render(sliced, found);
-            console.log(this.current_shown_items);
-            console.log(products.length);
             if (this.current_shown_items == products.length) {
                 return true;
             }
         },
     }
+    
     product.init();
+
+    var cart = {
+        products_in_cart: [],
+        
+        init: function() {
+            this.bindEvent();
+        },
+        
+        bindEvent: function() {
+            //ao clicar em comprar adicionar produto no carrinho
+            document.getElementById("products").onclick = function(event) {
+                if (event.target.classList.contains("comprar")) {
+                    cart.products_in_cart.push(event.target.value);
+                    cart.renderCart();
+                }
+            };
+            
+        },
+
+        renderCart: function() {
+            document.getElementById("cart_count").innerHTML = this.products_in_cart.length;
+        },
+    }
+    cart.init();
 }
+
 
 function getProducts () {
     let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
-             if (request.status == 200) {
+            if (request.status == 200) {
                 var data = JSON.parse(request.responseText);
                 processProducts(data);
-                } else {
-                    console.log("Error " + request.status)
-                }
+            } else {
+                console.log("Error " + request.status)
             }
-        };
+        }
+    };
     request.open('get', 'http://localhost:5000/products', true);
     request.send();
 }
