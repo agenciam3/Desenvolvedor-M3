5fee2f370f638c3209ea517aa64a14fe9c26b46f
vgabriel04 <victor_gabriels04@hotmail.com>
2022-03-01 16:12:06 -0400

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-console.log("Dev m3");
+const APIURL = "http://localhost:5000/products";
+
+// const getProducts = () => {
+//   axios
+//     .get(APIURL)
+//     .then((response) => {
+//       console.log(response.data);
+//     })
+//     .catch((error) => console.log(error));
+// };
+// getProducts();
+
+// --------- ACESSANDO O FORM COLORS COM OS INPUTS DE CADA COR
+const colors = document.querySelectorAll("#colors");
+const newColors = colors.map(element => console.log(element))
+console.log(colors[0]);
+
+function newColor(colorSelected) {
+  return colorSelected
+}
