928f5f440720453a63f95c49fa701acdbcf59f12
Sabrina Sampaio <sassa.sampaio@gmail.com>
2022-03-02 18:30:29 -0300

    fixed filters

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
-console.log("Dev m3");
 main();
 
 var productsArray;
-var filteredProducts = new Array;
+var filtered = new Array;
+var ordered = new Array;
 var filtersApplyed = new Array;
 
-function filterPrice(product, key){  
+function filterPrice(product, key){ 
     if(key == "0-to-50"){
         return (product.price>= 0 && product.price<=50) ? true : false; 
     }else if (key == "51-to-150"){
@@ -15,28 +15,67 @@ function filterPrice(product, key){
     }else if(key == "301-to-500"){
         return (product.price>= 301 && product.price<=500) ? true : false; 
     }else{
-        return (product.price<500) ? true : false; 
+        return (product.price>501) ? true : false; 
     }
 }
 
-function applyFilter(filtersApplyed){ 
-    filteredProducts = new Array
-    filtersApplyed.forEach(elem => {
-        if(elem[0] == 'color'){      
-            filteredProducts = filteredProducts.concat(productsArray.filter(produto => produto.color == elem[1]));
-        }else if(elem[0] == 'size'){
-            filteredProducts = filteredProducts.concat(productsArray.filter(produto => produto.size == elem[1]));
-        }else{
-            filteredProducts = filteredProducts.concat(productsArray.filter(elem => filterPrice(elem, elem[0])));
-        }    
-    });
-    document.getElementById("products").innerHTML = "";
-    showProducts(filteredProducts)
+function filterSize(product, key){
+    if(product.size.indexOf(key) == -1)
+        return false;
+    else
+        return true;
 }
 
-function removeFilter(param){
-    filtersApplyed = filtersApplyed.filter(filter => filter[1] !== param[1])
-    console.log(filtersApplyed.length)
+function applyFilter(filtersApplyed){ 
+    filteredProducts = new Array 
+    var price = false;
+    var size = false;
+    var color = false;
+
+    filtersApplyed.sort(function compare(a, b) {
+        if (a[0] < b[0]) return -1;
+        if (a[0] > b[0]) return 1;
+        return 0;
+    })
+
+    filtersApplyed.forEach(elem => {
+        if(elem[0] == 'color'){
+            color = true; 
+            if(price || size){ 
+                filteredProducts = filteredProducts.filter(produto => produto.color == elem[1]);          
+            }else{  
+                filteredProducts = filteredProducts.concat(productsArray.filter(produto => produto.color == elem[1]));            
+            } 
+        }else if(elem[0] == 'size'){
+            size = true;
+            if(price || color){
+                filteredProducts = filteredProducts.filter(element =>filterSize(element, elem[1]));       
+            }else{ 
+                filteredProducts = filteredProducts.concat(productsArray.filter(element =>filterSize(element, elem[1])));       
+            }
+        }else{
+            price = true;
+            if(size || color){ 
+                filteredProducts = filteredProducts.filter(element => filterPrice(element, elem[1]));           
+            }else{
+                filteredProducts =filteredProducts.concat(productsArray.filter(element => filterPrice(element, elem[1])));            
+            }         
+        }    
+    });
+
+    filtered = filteredProducts;
+
+    if(ordered.length == 0){
+        document.querySelector(".products").innerHTML = ""; 
+        showProducts(filteredProducts)
+
+    }else{
+        orderProducts(ordered[0][0], ordered[0][1])
+    }
+}
+
+function removeFilter(param){   
+    filtersApplyed = filtersApplyed.filter(filter => filter[1] != param[1])
     if(filtersApplyed.length === 0){
         document.getElementById("products").innerHTML = "";
         showProducts(productsArray)
@@ -56,7 +95,7 @@ function checkbox(){
                 if(elem.getAttribute("id")=='color'){
                     removeFilter([elem.getAttribute("id"), elem.getAttribute("color")])
                 }else{
-                    removeFilter([elem.getAttribute("price")])       
+                    removeFilter([elem.getAttribute("id"), elem.getAttribute("price")])       
                 }                
             }else{
                 checked.style.display = 'block';
@@ -70,6 +109,26 @@ function checkbox(){
             }
         };
     });
+}
+
+function checkboxBtn(){
+    var btn = Array.from(document.querySelectorAll(".checkbox-btn"));
+    btn.map(elem => {
+        elem.onclick = function (e) { 
+            if(elem.getAttribute('active')=='false'){
+                elem.setAttribute('active','true');
+                elem.style.border = "1.5px solid #00c0ee";
+                filtersApplyed.push(["size",elem.getAttribute('size')])
+                applyFilter(filtersApplyed)
+            }else{
+                elem.setAttribute('active','false');
+                elem.style.border = "1.5px solid gray";
+                removeFilter(['size', elem.getAttribute("size")])
+            }
+        };
+    });
+    
+
 }
 
 function getAdditionalColors(){
@@ -88,7 +147,8 @@ function getAdditionalColors(){
 }
 
 function orderProducts(type, asc){
-    var orderedArray = productsArray;
+    var orderedArray = filtered.length === 0 ? orderedArray : filtered;
+
     if(asc){
         orderedArray.sort(function compare(a, b) {
             if (a[type] < b[type]) return -1;
@@ -102,6 +162,7 @@ function orderProducts(type, asc){
             return 0;
         })
     } 
+    
     document.getElementById("products").innerHTML = "";
     showProducts(orderedArray);
 }
@@ -126,12 +187,18 @@ function selector(){
             options.style.display = 'none';
             btn.childNodes[0].data = elem.childNodes[0].data;
             let orderType = elem.getAttribute("ordertype");
-
+            ordered.length = 0;
             if(orderType == "recent"){
-                orderProducts("date",true);
+                //ordered.length = 0;
+                ordered.push(["date",false]);
+                orderProducts("date",false);
             }else if(orderType == "low-price"){
+                //ordered.length = 0;
+                ordered.push(["price",true]);
                 orderProducts("price",true);
             }else{
+                //ordered.length = 0;
+                ordered.push(["price",false]);
                 orderProducts("price",false)
             }
         };
@@ -139,19 +206,24 @@ function selector(){
 }
 
 function getProducts(){
-    var requestURL = 'http://localhost:5000/products';
-    var request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function() {
-        productsArray = request.response;
-        showProducts(productsArray);
-    }
+    fetch('http://localhost:5000/products')
+        .then(res => res.json())
+        .then((res) => {
+           productsArray = res;
+           showProducts(productsArray);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
-function showProducts(products){
-    console.log("entrou aqui",products)
+function showProducts(products){   
+    if(products.length > 9){
+        showMoreProducts(products)
+        document.querySelector(".load-more-btn").style.display = 'block' 
+    }else{
+        document.querySelector(".load-more-btn").style.display = 'none'
+    }
     products = products.slice(0,9);
     products.map(elem => {
         let listAllProducts = document.getElementById("products");
@@ -168,9 +240,30 @@ function showProducts(products){
         `
 
         listAllProducts.insertAdjacentHTML('beforeend', htmlInsert); 
-        filteredProducts = productsArray
-        addToCart();   
+        addToCart();  
     });
+}
+
+function showMoreProducts(products){
+    products = products.slice(9,-1);
+    document.querySelector(".load-more-btn").onclick = function(){
+        products.map(elem => {
+            let listAllProducts = document.getElementById("products");
+            let htmlInsert = `
+                <div class="product">
+                    <img src="${elem.image}" alt="erro">
+                    <h3>${elem.name.toUpperCase()}</h3>
+                    <span style="font-weight:bold">R$${Number(elem.price).toFixed(2)} </span>
+                    <span style="font-weight: 300;font-size: 15px;">até ${elem.parcelamento[0]}x de R$${Number(elem.parcelamento[1]).toFixed(2)} </span>
+                    <button id="add" class="add" productId=${elem.id} price=${elem.price}>
+                        COMPRAR
+                    </button>   
+                </div>
+            `
+            listAllProducts.insertAdjacentHTML('beforeend', htmlInsert);
+        });
+        document.querySelector(".load-more-btn").style.display = 'none';
+    }
 }
 
 function addToCart(){
@@ -239,6 +332,7 @@ function main(){
     getProducts();
     localStorage.clear();
     checkbox();
+    checkboxBtn();
     selector();
     getAdditionalColors();
     orderMobile();
