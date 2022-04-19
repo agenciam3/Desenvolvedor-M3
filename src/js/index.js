4a3c803e5354a7708ec28fea01855a2b254c3a87
Geovanne <callegarog00@gmail.com>
2022-04-19 10:44:59 -0300

    changing way to get data and function to load more posts

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,113 @@
 import 'regenerator-runtime/runtime';
 
+let productsPerPage = 4
+
+//  function to get all products
 const getAllProducts = async () => {
   const productsFetch = await fetch('http://localhost:5000/products');
   const productsJson = await productsFetch.json();
   return productsJson
 };
 
+// function to generate html for all products 
 const generateHtmlProduct = products => {
+  const productsMap = []
 
-  const productsReduce = products.reduce((accumulator, {name, price, parcelamento, image}) => {
-    accumulator += 
-      `
-        <div class="products--cardItem">
-          <div class="products--imageCardItem">
-            <img src=${image} alt="">
-          </div>
-          <div class="products--textInformationCardItem">
-            <h3>${name}</h3>
-            <p class="bold">R$ ${price}</p>
-            <p class="text-installments">até ${parcelamento[0]}x de ${parcelamento[1]}</p>
-          </div>
-          <div class="products--buttonBuyCardItem">
-            <button>Comprar</button>
-          </div>
-        </div>
-      `
-    return accumulator
-  }, '')
-
-  return productsReduce
+  // // push the array of products with the result of the products
+  const productsReduce = products.map((product) => {
+    productsMap.push(product)
+  })
+  
+  return productsMap
 }
 
+// function to insert html in page for all products
 const insertProductsIntoPage = htmlProduct => {
   const divProductsContainer = document.querySelector('.products--cardContainer')
-  divProductsContainer.innerHTML = htmlProduct
+  
+  // generate html for inner 
+  htmlProduct.map((product) => {
+    divProductsContainer.innerHTML += `
+      <div class="products--cardItem ${product.id}">
+        <div class="products--imageCardItem">
+          <img src=${product.image}></img>
+        </div>
+        <div class="products--textInformationCardItem">
+          <h3>${product.name}</h3>
+          <p class="bold">${product.price}</p>
+          <p class="text-installments">até ${product.parcelamento[0]}x de R$${product.parcelamento[1]}</p>
+        </div>
+      </div>
+    `
+  })
+
+  // checking the size of the user's screen and adjusting the amount of initial products
+  if(window.innerWidth < 425) {
+    for(let i = productsPerPage; i < divProductsContainer.children.length; i++) {
+      divProductsContainer.children[i].style.display = 'none'
+    }
+  } else if(window.innerWidth > 520 && window.innerWidth < 1000) {
+    productsPerPage = 6
+    for(let i = productsPerPage; i < divProductsContainer.children.length; i++) {
+      divProductsContainer.children[i].style.display = 'none'
+    }
+  } else if(window.innerWidth > 1000) {
+    productsPerPage = 9
+    for (let i = productsPerPage; i < divProductsContainer.children.length; i++) {
+      divProductsContainer.children[i].style.display = 'none'
+    }
+  }
 }
 
+// function loadMorePostsButton
+const buttonLoadMore = document.querySelector('#buttonLoadMore')
+
+const loadMoreProducts = async () => {
+  const divProductsContainer = document.querySelector('.products--cardContainer')
+  const productsInPage = []
+  const productsOutPage = []
+
+  // loop to fill the arrays of products that are displayed and that are not displayed
+  for(let product of divProductsContainer.children) {
+    if(product.style.display != 'none') {
+      productsInPage.push(product)
+    } else {
+      productsOutPage.push(product)
+    }
+  }
+
+  // checking the user's screen size and adding products according to that size in the load products function
+  if(window.innerWidth < 425) {
+    productsInPage.slice(productsInPage, productsInPage + productsOutPage.map((product, index) => {
+      if (index < 1) {
+        product.style.display = 'flex'
+      }
+      
+    }))
+  } else if (window.innerWidth > 500 && window.innerWidth < 1000) {
+    productsInPage.slice(productsInPage, productsInPage + productsOutPage.map((product, index) => {
+      if (index < 2) {
+        product.style.display = 'flex'
+      }
+    }))
+  } else if (window.innerWidth > 1000) {
+    productsInPage.slice(productsInPage, productsInPage + productsOutPage.map((product, index) => {
+      if (index < 3) {
+        product.style.display = 'flex'
+      }
+    }))
+  }
+}
+
+// add function in button Load More
+buttonLoadMore.addEventListener('click', loadMoreProducts) 
+
+// function to resolve promise products 
 const promisesResolve = async () => {
   const products = await getAllProducts()
   const htmlProduct = await generateHtmlProduct(products)
-  const insertHtml = insertProductsIntoPage(htmlProduct)
+  insertProductsIntoPage(htmlProduct)
 }
 
+// call all promise resolves function
 promisesResolve()
