efcf624b3fdf17b4775717be4f2f7a660c02d0fe
leon-siqueira <leon.siqueir4@gmail.com>
2022-04-25 15:43:04 -0300

    product cards created

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,35 @@
-console.log("Dev m3");
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+var products = document.getElementById('products');
+console.log(products)
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'http://localhost:5000/products', true)
+
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  data.forEach(product => {
+    console.log(product.parcelamento)
+  // Log each movie's title
+  const productCard =
+  `<div class="product">
+    <img class="product-img" src="${product.image}" alt="">
+    <p class="product-name">${product.name}</p>
+    <p class="price">R$${parseFloat(product.price).toFixed(2)}</p>
+    <p class="parcelamento">Até ${product.parcelamento[0]}x de R$${parseFloat(product.parcelamento[1]).toFixed(2)}</p>
+    <div class="comprar">
+      COMPRAR
+    </div>
+  </div>`;
+  products.insertAdjacentHTML("beforeend", productCard)
+})
+}
+
+// Send request
+request.send()
+
+
+function insert_products(products) {
+
+}
