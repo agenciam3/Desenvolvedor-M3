04afd3b6e766469af0ce59152909439b26d3cb7c
squad11recodepro <caiostun@gmail.com>
2022-04-27 11:56:21 -0300

    Atualização 27/04/22 11:56

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,41 @@
-/* const fetchApi = () => {
-  const url = `http://localhost:5000/products`;
+// Para cada usuario
+// criar uma linha
+// adicionar na tabela
 
-  fetch(url).then((response) => response.json());
-};
+function fazGet(url) {
+  let request = new XMLHttpRequest();
+  request.open("GET", url, false);
+  request.send();
+  return request.responseText;
+}
 
-fetchApi();
+function criarLinha(pecas) {
+  linha = document.createElement("tr");
+  tdId = document.createElement("td");
+  tdName = document.createElement("td");
+  tdPrice = document.createElement("td");
+  tdId.innerHTML = pecas.id;
+  tdName.innerHTML = pecas.name;
+  tdPrice.innerHTML = pecas.price;
 
-console.log("Dev m3");
- */
+  linha.appendChild(tdId);
+  linha.appendChild(tdName);
+  linha.appendChild(tdPrice);
+
+  return linha;
+}
+
+function main() {
+  let data = fazGet("http://localhost:5000/products");
+  let pecas = JSON.parse(data);
+  let tabela = document.getElementById("tabela");
+
+  pecas.forEach((element) => {
+    let linha = criarLinha(element);
+    tabela.appendChild(linha);
+  });
+}
+
+main();
+
+console.log(main);
