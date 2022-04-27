aefbaa074f57873017a8eaf719ff26218c67f023
leon-siqueira <leon.siqueir4@gmail.com>
2022-04-27 20:02:55 -0300

    refactor(index.js): comments from the old api accessing method removed

--- src/js/index.js
+++ src/js/index.js
@@ -3,37 +3,7 @@ import { addColorsFilter, listColors, checkUncheck } from "./navbar/colors";
 import { radioCheckUncheck } from "./navbar/prices";
 import { addCardProducts } from "./module_products";
 
-
-// Create a request variable and assign a new XMLHttpRequest object to it.
-// let request = new XMLHttpRequest()
 let url = 'http://localhost:5000/products';
-
-// request.open('GET', 'http://localhost:5000/products', true)
-
-// request.onload = function () {
-//   // Begin accessing JSON data here
-
-//   let response = JSON.parse(this.response)
-//   add_card_products(response)
-//   add_colors_filter(list_colors(response))
-//   add_size_buttons(list_sizes(response))
-//   let colorCheckBoxes = document.querySelectorAll('.color-checkbox')
-//   colorCheckBoxes.forEach(checkBox => {
-//     checkBox.addEventListener("click", check(checkBox))
-//   });
-//   function sayHello(checkbox) {
-//     if (checkbox.checked) {
-//       console.log("hello")
-//     }
-//   }
-// }
-
-
-// // Send request
-// request.send()
-
-
-
 
 fetch(url)
   .then(response => response.json())
