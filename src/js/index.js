8953d5860c9d32045f1ff9b5400a1faa2c4c9ad6
squad11recodepro <caiostun@gmail.com>
2022-04-29 11:52:17 -0300

    Atualização 11:52

--- src/js/index.js
+++ src/js/index.js
@@ -63,22 +63,3 @@ function createCard(product, index) {
 }
 
 getProducts();
-
-// Função do Dropdown
-
-function OpenDropDown() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches(".dropbtn")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-};
