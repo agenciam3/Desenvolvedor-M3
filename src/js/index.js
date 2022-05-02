b39709682e15c618fe02255aa6b1b5d89383ba52
Braien Machado de Paula <braienmp@outlook.com>
2022-05-02 16:44:57 -0300

    implementa lógica de carregar novos itens

--- src/js/index.js
+++ src/js/index.js
@@ -30,11 +30,29 @@ function createCards(items) {
   });
 }
 
+function loadMoreContent() {
+  let hiddenItems = document.querySelectorAll('.hidden-mobile');
+
+  hiddenItems.forEach((item, i) => {
+    if (i < 4)
+    item.classList.remove('hidden-mobile');
+  });
+
+  hiddenItems = document.querySelectorAll('.hidden-mobile');
+
+  if (hiddenItems.length === 0) {
+    const loaderButton = document.getElementById('loader-button');
+    loaderButton.style.display = 'none';
+  }
+}
+
 function createLoaderButton() {
   const loaderButton = document.createElement('button');
 
   loaderButton.innerText = 'Carregar mais';
   loaderButton.id = 'loader-button';
+
+  loaderButton.addEventListener('click', loadMoreContent);
 
   const cardsContainer = document.getElementById('cards-container');
 
