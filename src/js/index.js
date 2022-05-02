04517e5815b9995e74b2c558a6dad6f3e42f0332
Braien Machado de Paula <braienmp@outlook.com>
2022-05-02 15:32:12 -0300

    create products' cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-console.log("Dev m3");
+import axios from 'axios';
+
+function createCard(item) {
+  const innerHTML = `
+    <img src=${item.image} alt=${item.name} />
+    <h2>${item.name}</h2>
+    <h3>R$ ${item.price.toFixed(2)}</h3>
+    <p>até ${item.parcelamento[0]}x de R$${item.parcelamento[1].toFixed(2)}</p>
+    <button>COMPRAR</button>
+    `;
+
+  const cardWrapper = document.createElement('div');
+  cardWrapper.className = 'card-wrapper';
+  cardWrapper.innerHTML = innerHTML;
+
+  const cardsContainer = document.getElementById('cards-container');
+
+  cardsContainer.appendChild(cardWrapper);
+}
+
+function createCards(items) {
+
+  items.forEach((item) => {
+    createCard(item);
+  });
+}
+
+axios.get('http://localhost:5000/products')
+  .then(({ data }) => createCards(data));
