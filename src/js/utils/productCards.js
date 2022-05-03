58f64364b0b9850beef6b0e3b4c05f6b887cc7d8
Braien Machado de Paula <braienmp@outlook.com>
2022-05-02 21:04:59 -0300

    Mostra quantos produtos foram adicionados ao carrinho

--- src/js/utils/productCards.js
+++ src/js/utils/productCards.js
@@ -1,20 +1,42 @@
+function addToCart() {
+  let counter = document.getElementById('cart-counter');
+  
+  if (!counter) {
+    counter = document.createElement('span');
+    counter.id = 'cart-counter';
+    counter.innerHTML = 1;
+
+  } else {
+    counter.innerHTML = parseInt(counter.innerHTML, 10) + 1;
+  }
+
+  const header = document.querySelector('header');
+  header.appendChild(counter);
+}
+
 function createCard(item, index) {
   const innerHTML = `
     <img src=${item.image} alt=${item.name} />
     <h2>${item.name}</h2>
     <span>R$ ${item.price.toFixed(2)}</span>
     <p>até ${item.parcelamento[0]}x de R$${item.parcelamento[1].toFixed(2)}</p>
-    <button>COMPRAR</button>
     `;
 
   const cardWrapper = document.createElement('div');
   cardWrapper.className = 'card-wrapper';
-
+  
   if (window.innerWidth <= 700 && index > 3) {
     cardWrapper.classList.add('hidden-mobile');
+  } else if (index > 5) {
+    cardWrapper.classList.add('hidden-mobile');
   }
-
+  
   cardWrapper.innerHTML = innerHTML;
+
+  const button = document.createElement('button');
+  button.textContent = 'COMPRAR';
+  button.addEventListener('click', addToCart);
+  cardWrapper.appendChild(button);
 
   const cardsContainer = document.getElementById('cards-container');
 
