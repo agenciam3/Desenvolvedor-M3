0f2482876751c941be1ab6995b8c98042239e6f5
Leonardo oliveira dos santos <leonardo.santos@kafnet.com.br>
2022-10-22 12:25:30 -0300

    implementação de layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,35 @@
-const serverurl = process.env.SERVER_API;
+import "regenerator-runtime/runtime";
 
-console.log("Dev m3", serverurl);
+
+// LISTAR PRODUTOS
+const url = "http://localhost:5000/products";
+const productGrid = document.querySelector("#productListContent");
+async function getAllProducts() {
+  const response = await fetch(url);
+  const data = await response.json();
+  const initialProducts = data.slice(0, 9)
+  const renderProducts = initialProducts.map((product) => {
+    productGrid.innerHTML+= 
+    `<article class="product" id=${product.id}>
+    <img src=${product.image} class="product__image" alt=${product.name} title=${product.name}/>
+    <h2 class="product__name">${product.name}</h2>
+     <div class="product__details">
+      <span class="product__price">R$ ${product.price}</span>
+      <span class="product__parcelamento">até ${product.parcelamento[0]}x de R$${product.parcelamento[1]}</span>
+      </div>
+      <button class="button" type="button">Comprar</button>
+    </article>
+   `
+});
+
+}
+
+
+
+window.addEventListener('DOMContentLoaded', async function () {
+       getAllProducts();
+});
+
+
+
+
