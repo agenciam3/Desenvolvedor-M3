97d4159fe4ccc91051a0fb42c28c06cb5442601e
Nicholas Costa <nicholascostadev@gmail.com>
2022-11-13 16:28:54 -0300

    ✨  Join all logic for listing products + filter with colors + ordering

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,52 @@
+import addEventListeners, { listenToProductsButton } from "./eventListeners";
+import { api } from "./utils/api";
+import { generateCards } from "./utils/products";
+
+addEventListeners();
+export let products = [];
+let from = 0;
+let offset = 9;
 let filteredProducts = [];
+export let selectedProducts = [];
+
+api.get("/products").then((res) => {
+  products = filteredProducts = res.data;
+  renderProducts();
+  listenToProductsButton();
+});
+
 export function setFilterdProducts(filtered) {
   filteredProducts = filtered;
 }
 
-const serverurl = process.env.SERVER_API;
+export function renderProducts() {
+  const productsContainer = document.querySelector(".products-container");
+  const productsToShow = filteredProducts.slice(
+    from,
+    Math.min(products.length, offset)
+  );
 
-addEventListeners();
+  productsContainer.innerHTML = "";
+
+  if (productsToShow.length === 0) {
+    productsContainer.innerHTML = `
+      <h2 class="text-center">Nenhum produto encontrado com esses filtros</h2>
+    `;
+  } else {
+    generateCards(productsToShow);
+  }
+
+  if (filteredProducts.length <= offset) {
+    document.querySelector(".load-more").classList.add("hidden");
+    return;
+  }
+
+  document.querySelector(".load-more").classList.remove("hidden");
+  document.querySelector(".loader").classList.add("hidden");
+}
+
+export function loadMore() {
+  offset += offset;
+  renderProducts();
+  listenToProductsButton();
+}
