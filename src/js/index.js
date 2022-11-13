856d7e4c1c82bc27cf4b8ae1be01bb42f23bf728
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-11-13 13:52:30 -0300

    feat: Adicionando produtos da API

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,48 @@
 const serverurl = process.env.SERVER_API;
 
-console.log("Dev m3", serverurl);
+let product = document.querySelector(".product-list-content");
+
+function createProduct(data) {
+  console.log(data.name);
+  productDiv = document.createElement("div");
+  productImage = document.createElement("img");
+  productName = document.createElement("h3");
+  productPrice = document.createElement("p");
+  buyButton = document.createElement("button");
+  productImage.src = data.image;
+
+  productName.innerHTML = data.name;
+  productPrice.innerHTML = data.price.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  buyButton.innerHTML = "Comprar";
+
+  productDiv.appendChild(productImage);
+  productDiv.appendChild(productName);
+  productDiv.appendChild(productPrice);
+  productDiv.appendChild(buyButton);
+
+  return productDiv;
+}
+
+function main() {
+  fetch("http://localhost:5000/products")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      data.forEach((element) => {
+        let productDiv = createProduct(element);
+
+        product.appendChild(productDiv);
+      });
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
+}
+
+main();
