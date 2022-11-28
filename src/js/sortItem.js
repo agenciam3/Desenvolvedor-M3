936f79c8191bf25bbdd00403aa940a37ca197d8e
João Paulo <jpgoncalves021@gmail.com>
2022-11-28 01:54:55 -0300

    Changes in sort item function.

--- src/js/sortItem.js
+++ src/js/sortItem.js
@@ -1,30 +1,26 @@
 /* FUNÇÕES PARA ORGANIZAR OS OBJETOS 
 POR DATA, PREÇO MAIS ALTO E PREÇO MAIS BAIXO
 */
+
+
 async function sortLower() {
     await fetch('http://localhost:5000/products?_sort=price&_order=asc')
         .then((resp) => resp.json())
         .then((data) => {
-            data.map(() => {
-                renderProduct(data)
-            })
+            renderProduct(data)
         });
 }
 async function sortHigh() {
     await fetch('http://localhost:5000/products?_sort=price&_order=desc')
         .then((resp) => resp.json())
         .then((data) => {
-            data.map(() => {
-                renderProduct(data)
-            })
+            renderProduct(data)
         });
 }
 async function sortDate() {
     await fetch('http://localhost:5000/products?_sort=date&_order=desc')
         .then((resp) => resp.json())
         .then((data) => {
-            data.map(() => {
-                renderProduct(data)
-            })
+            renderProduct(data)
         });
 }
