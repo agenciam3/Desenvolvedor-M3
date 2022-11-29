ba2dfa8af0a3038825e6745cf638645bec2805ff
pedro772 <plucena17230@gmail.com>
2022-11-28 21:34:01 -0300

    feat: add sort data filter

--- src/js/App.js
+++ src/js/App.js
@@ -12,6 +12,11 @@ export default class App extends HTMLElement {
       priceRanges: []
     }
 
+    this.dataToSort = {
+      dates: [],
+      prices: []
+    }
+
     this.filteredData = [];
 
     const shadow = this.attachShadow({ mode: "open" });
@@ -34,6 +39,57 @@ export default class App extends HTMLElement {
     this.updateComponent(this);
   }
 
+  sortFilter(sortBy, node) {
+    let sortedData = [];
+
+    switch (sortBy) {
+      case "Mais recentes":
+        this.state.data.forEach(product => this.dataToSort.dates.push(product.date));
+        this.dataToSort.dates = this.dataToSort.dates.sort((a, b) => a - b);
+
+        this.dataToSort.dates.forEach(date => {
+          this.state.data.forEach(product => {
+            if(product.date === date) {
+              sortedData.push(product);
+            }
+          })
+        })
+        break;
+
+      case "Menor preço":
+        this.state.data.forEach(product => this.dataToSort.prices.push(product.price));
+        this.dataToSort.prices = this.dataToSort.prices.sort((a, b) => a - b);
+
+        this.dataToSort.prices.forEach(price => {
+          this.state.data.forEach(product => {
+            if(product.price === price) {
+              sortedData.push(product);
+            }
+          })
+        })
+        break;
+
+      case "Maior preço":
+        this.state.data.forEach(product => this.dataToSort.prices.push(product.price));
+        this.dataToSort.prices = this.dataToSort.prices.sort((a, b) => a - b);
+
+        this.dataToSort.prices.forEach(price => {
+          this.state.data.forEach(product => {
+            if(product.price === price) {
+              sortedData.unshift(product);
+            }
+          })
+        })
+        break;
+
+      default:
+        break;
+    }
+
+    sortedData = new Set(sortedData);
+    const uniqueSortedData = Array.from(sortedData);
+    node.data = uniqueSortedData;
+  }
 
   filter(value, valueIsChecked, filterType, node) {
     this.filteredData = this.state.data;
@@ -111,13 +167,17 @@ export default class App extends HTMLElement {
     priceFilter.data = this.state.data;
     priceFilter.addEventListener("optionselected", (e) => this.filter(e.detail.value, e.detail.isChecked, "priceRange", productsContainer));
 
+    const sortFilter = document.createElement("sort-filter");
+    sortFilter.addEventListener("optionselected", (e) => this.sortFilter(e.detail.value, productsContainer));
+
     filtersForm.appendChild(colorFilter);
     filtersForm.appendChild(sizeFilter);
     filtersForm.appendChild(priceFilter);
 
     appContainer.appendChild(navbar);
-    appContainer.appendChild(productsContainer);
     appContainer.appendChild(filtersForm);
+    appContainer.appendChild(productsContainer);
+    appContainer.appendChild(sortFilter);
     appContainer.appendChild(footer);
   }
 }
---
+++ src/js/filters/sortFilter/SortFilter.js
@@ -0,0 +1,53 @@
+import { allSortOptions } from "../../utils";
+
+export default class SortFilter extends HTMLElement {
+  constructor() {
+    super();
+
+    this._data = {};
+  }
+
+  set data(value) {
+    this._data = value;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  connectedCallback() {
+    this.updateComponent(this);
+  }
+
+  dispatchOptionSelectedEvent(event, node) {
+    const optionSelected = new CustomEvent('optionselected', {
+      detail: {
+        value: event.target.value
+      }
+    })
+
+    node.dispatchEvent(optionSelected)
+  }
+
+  updateComponent(node) {
+    const sortSelect = document.createElement("select");
+    node.append(sortSelect);
+
+    const sortOption = document.createElement("option");
+    sortOption.setAttribute("value", "Ordenar por:");
+    sortOption.setAttribute("selected", true);
+    sortOption.setAttribute("disabled", true);
+    sortOption.setAttribute("hidden", true);
+    sortOption.innerHTML = "Ordenar por:";
+    sortSelect.appendChild(sortOption);
+
+    allSortOptions.map(option => {
+      const sortOption = document.createElement("option");
+      sortOption.setAttribute("value", option);
+      sortOption.innerHTML = option;
+      sortSelect.appendChild(sortOption);
+    });
+
+    sortSelect.addEventListener("change", (event) => this.dispatchOptionSelectedEvent(event, node))
+  }
+}
