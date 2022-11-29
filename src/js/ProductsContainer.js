bd891850f2cb7dec3f5323b938a90a20fd3fc067
pedro772 <plucena17230@gmail.com>
2022-11-29 13:06:24 -0300

    feat: add information for no data found

--- src/js/ProductsContainer.js
+++ src/js/ProductsContainer.js
@@ -20,13 +20,20 @@ export default class ProductsContainer extends HTMLElement {
 
   updateComponent(node) {
     node.innerHTML = "";
+    
+    if(this.data.length > 0) {
+      this.data.map(productData => {
+        const productCard = document.createElement("product-card");
+        productCard.classList.add("product");
+        productCard.data = productData;
 
-    this.data.map(productData => {
-      const productCard = document.createElement("product-card");
-      productCard.classList.add("product");
-      productCard.data = productData;
-
-      node.appendChild(productCard);
-    })
+        node.appendChild(productCard);
+      })
+    } else {
+      const noDataFound = document.createElement("h2");
+      noDataFound.classList.add("no-data__text");
+      noDataFound.textContent = "Nenhum produto encontrado";
+      node.appendChild(noDataFound);
+    }
   }
 }
