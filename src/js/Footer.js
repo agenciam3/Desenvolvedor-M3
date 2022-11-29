1836712faafb297062283b5344974145e2012712
pedro772 <plucena17230@gmail.com>
2022-11-29 13:15:09 -0300

    style: style footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -9,7 +9,9 @@ export default class Footer extends HTMLElement {
 
   updateComponent(node) {
     const footer = document.createElement("footer");
+    footer.classList.add("footer");
     const label = document.createElement("label");
+    label.classList.add("footer__label");
     label.innerHTML = "M3: IMPLEMENTAÇÃO DE E-COMMERCE VTEX";
     
     footer.appendChild(label);
