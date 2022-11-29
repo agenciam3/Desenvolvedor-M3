4a83144e3c866f4d5965bf1941d735af08ed8ea5
pedro772 <plucena17230@gmail.com>
2022-11-28 22:04:09 -0300

    style(navbar): add styles

--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -9,12 +9,15 @@ export default class Navbar extends HTMLElement {
 
   updateComponent(node) {
     const navbar = document.createElement("nav");
+    navbar.classList.add("navbar");
 
     const logo = document.createElement("img");
     logo.setAttribute("src", "../img/logo-m3.png");
+    logo.classList.add("navbar__first-element")
 
     const cart = document.createElement("img");
     cart.setAttribute("src", "../img/cart.png");
+    cart.classList.add("navbar__second-element")
     
     navbar.appendChild(logo);
     navbar.appendChild(cart);
