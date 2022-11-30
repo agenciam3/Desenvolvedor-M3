8da041123cea6e14521691df491166e251b495f2
pedro772 <plucena17230@gmail.com>
2022-11-29 22:33:19 -0300

    feat: expand colors filter options

--- src/js/filters/colorFilter/ColorFilter.js
+++ src/js/filters/colorFilter/ColorFilter.js
@@ -41,5 +41,25 @@ export default class ColorFilter extends HTMLElement {
       colorOption.addEventListener("optionselected", (e) => this.dispatchOptionSelectedEvent(e, node));
       colorFilterContainer.appendChild(colorOption);
     });
+
+    const seeAllColors = document.createElement("div");
+    seeAllColors.classList.add("color-filter-expand");
+
+    const seeAllColorsSpan = document.createElement("span");
+    seeAllColorsSpan.innerHTML = "Ver todas as cores";
+    seeAllColorsSpan.classList.add("color-filter-expand__text");
+    seeAllColors.appendChild(seeAllColorsSpan);
+
+    const seeAllColorsIcon = document.createElement("img");
+    seeAllColorsIcon.setAttribute("src", "../../img/color_filter_expand_icon.svg");
+    seeAllColors.appendChild(seeAllColorsIcon);
+
+    seeAllColors.addEventListener("click", (e) => {
+      e.preventDefault();
+      node.querySelectorAll(".filter__container color-option").forEach(el => el.style.display = "block");
+      seeAllColors.style.display = "none";
+    })
+
+    node.appendChild(seeAllColors) 
   }
 }
