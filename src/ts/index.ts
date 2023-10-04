0b07d1903f741900e96c1b22472bdb4d84d1b626
Patrick <patrickconservatorio@gmail.com>
2023-10-03 21:56:34 -0300

    feat: init commit

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,3 +7,20 @@ function main() {
 }
 
 document.addEventListener("DOMContentLoaded", main);
+
+const apiUrl = "http://localhost:5000/products";
+
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Erro ao recuperar os dados da API");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Foi");
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Erro:", error);
+  });
