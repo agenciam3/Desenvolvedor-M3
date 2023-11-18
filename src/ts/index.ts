753d1fb45ff2e18da9492553d314a2dba4aaee09
laynamoraes <laynamoraaes@hotmail.com>
2023-11-18 17:22:25 -0300

    criação da função de renderização dos produtos

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,10 +2,57 @@ import { Product } from "./Product";
 
 const serverUrl = "http://localhost:5000/products";
 
+const productsSection = document.getElementById("products");
+
+function createProduct(data: Product) {
+  const productContainer = document.createElement("article");
+  const productImage = document.createElement("img");
+  const productDescription = document.createElement("h3");
+  const productPrice = document.createElement("p");
+  const productInstallment = document.createElement("p");
+  const productButton = document.createElement("button");
+
+  productContainer.classList.add("product");
+  productImage.classList.add("product-image");
+  productDescription.classList.add("product-description");
+  productPrice.classList.add("product-price");
+  productInstallment.classList.add("product-installment");
+  productButton.classList.add("product-button");
+
+  productImage.src = data.image;
+  productDescription.innerHTML = data.name;
+  productPrice.innerHTML = data.price.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  productInstallment.innerHTML = `até ${
+    data.parcelamento[0]
+  }x de ${data.parcelamento[1].toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  })}`;
+  productButton.innerHTML = "COMPRAR";
+
+  productContainer.appendChild(productImage);
+  productContainer.appendChild(productDescription);
+  productContainer.appendChild(productPrice);
+  productContainer.appendChild(productInstallment);
+  productContainer.appendChild(productButton);
+
+  return productContainer;
+}
+
 function getDataFromApi() {
   fetch("http://localhost:5000/products")
     .then((data) => data.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      const productsData = data;
+
+      productsData.map((product: Product) => {
+        const productContainer = createProduct(product);
+        productsSection.appendChild(productContainer);
+      });
+    })
     .catch((error) => console.error(error));
 }
 
