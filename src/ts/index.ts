5900a1883d20761f5f5ea16e76bbfbcdfc565f74
will <wlymes@gmail.com>
2023-11-19 15:15:36 -0300

    feat: fetch cards

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,24 +2,6 @@ import { Product } from "./Product";
 
 const serverUrl = "http://localhost:5000";
 
-function main() {
-  console.log(serverUrl);
-}
-
-document.addEventListener("DOMContentLoaded", main);
-
-// constants ids
-const show_list_colors = document.querySelector("#show-icons-btn");
-const filters_btn = document.querySelector("#filters-btn");
-const order_btn = document.querySelector("#order-btn");
-const close_filters_btn = document.querySelector("#close-filters-btn");
-const close_order_btn = document.querySelector("#close-order-btn");
-
-// constants classes
-const show_list_items = document.querySelector(".show-list-items");
-const filter_order_mobile = document.querySelector(".filter-order-mobile");
-const filters_mobile = document.querySelector(".rest-filters-mobile");
-
 // data
 const colorsData = [
   { id: "orange", name: "Laranja" },
@@ -30,28 +12,39 @@ const colorsData = [
   { id: "wine", name: "Vinho" }
 ];
 
+// constants ids
+const show_list_colors = document.querySelector("#show-icons-btn");
+const filters_btn = document.querySelector("#filters-btn");
+const order_btn = document.querySelector("#order-btn");
+const close_filters_btn = document.querySelector("#close-filters-btn");
+const close_order_btn = document.querySelector("#close-order-btn");
+const load_btn = document.querySelector("#load-btn");
+
+// constants classes
+const show_list_items = document.querySelector(".show-list-items");
+const filter_order_mobile = document.querySelector(".filter-order-mobile");
+const filters_mobile = document.querySelector(".rest-filters-mobile");
+
+// fetch
+const getProducts = async () => {
+  const response = await fetch(`${serverUrl}/products`)
+  const data: Product[] = await response.json()
+  return data
+}
+
 // click events functions
 const showColorsList = () => {
   show_list_colors.classList.remove('show-icons-btn');
   show_list_colors.classList.add('hidden');
 
   colorsData.forEach(color => {
-    const inputContainer = document.createElement("div");
-    inputContainer.classList.add("input-container");
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = color.id;
-    checkbox.name = color.name;
-
-    const label = document.createElement("label");
-    label.htmlFor = color.id;
-    label.textContent = color.name;
-
-    inputContainer.appendChild(checkbox);
-    inputContainer.appendChild(label);
-
-    show_list_items.appendChild(inputContainer);
+    const inputContainer = `
+    <div class="input-container">
+    <input type="checkbox" id="${color.id}" name="${color.name}" />
+    <label for="${color.id}">${color.name}</label>
+  </div>
+    `
+    show_list_items.innerHTML += inputContainer;
   });
 }
 
@@ -79,6 +72,38 @@ const closeFiltersList = () => {
   }
 }
 
+const renderProducts = async (start: number, end: number) => {
+  try {
+    const products = await getProducts();
+    const gridContainer = document.querySelector(".content-grid-container");
+
+    for (let i = start; i < end && i < products.length; i++) {
+      const product = products[i];
+
+      const card = `
+        <div class="content-card">
+          <figure class="card-image-container">
+            <img src="${product.image}" alt="clothing-image">
+          </figure>
+
+          <div class="card-text-container">
+            <h2 class="card-clothing-name">${product.name}</h2>
+            <div class="card-span-container">
+              <span class="card-clothing-price">R$${product.price.toFixed(2)}</span>
+              <span class="card-clothing-divide-price">até ${product.parcelamento[0]}x de R$${(product.parcelamento[1] / product.parcelamento[0]).toFixed(2)}</span>
+            </div>
+            <button class="card-clothing-btn">Comprar</button>
+          </div>
+        </div>
+      `;
+
+      gridContainer.innerHTML += card;
+    }
+  } catch (error) {
+    console.error("Erro ao obter produtos:", error);
+  }
+};
+
 // click events
 // colors list
 show_list_colors.addEventListener('click', showColorsList);
@@ -89,3 +114,21 @@ filters_btn.addEventListener('click', showFiltersList);
 order_btn.addEventListener('click', showOrderList);
 close_order_btn.addEventListener('click', closeOrderList);
 close_filters_btn.addEventListener('click', closeFiltersList);
+
+load_btn.addEventListener("click", () => {
+  const lastRenderedIndex = document.querySelectorAll(".content-card").length;
+  renderProducts(lastRenderedIndex, lastRenderedIndex + 9);
+
+  if (load_btn) {
+    (load_btn as HTMLElement).style.display = 'none';
+  }
+});
+
+renderProducts(0, 9);
+
+// default
+function main() {
+  console.log(serverUrl);
+}
+
+document.addEventListener("DOMContentLoaded", main);
