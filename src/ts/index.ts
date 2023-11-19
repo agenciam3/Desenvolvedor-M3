7e8c1b42e135727cb645b8433de4b18c4e397f0b
will <wlymes@gmail.com>
2023-11-19 14:16:33 -0300

    feat: filters mobile

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,5 @@
 import { Product } from "./Product";
 
-
-
 const serverUrl = "http://localhost:5000";
 
 function main() {
@@ -11,12 +9,18 @@ function main() {
 document.addEventListener("DOMContentLoaded", main);
 
 // constants ids
-const btn_open = document.querySelector("#show-icons-btn");
+const show_list_colors = document.querySelector("#show-icons-btn");
+const filters_btn = document.querySelector("#filters-btn");
+const order_btn = document.querySelector("#order-btn");
+const close_filters_btn = document.querySelector("#close-filters-btn");
+const close_order_btn = document.querySelector("#close-order-btn");
 
 // constants classes
 const show_list_items = document.querySelector(".show-list-items");
+const filter_order_mobile = document.querySelector(".filter-order-mobile");
+const filters_mobile = document.querySelector(".rest-filters-mobile");
 
-
+// data
 const colorsData = [
   { id: "orange", name: "Laranja" },
   { id: "green", name: "Verde" },
@@ -26,10 +30,10 @@ const colorsData = [
   { id: "wine", name: "Vinho" }
 ];
 
-// click events
-function showColorsList() {
-  btn_open.classList.remove('show-icons-btn');
-  btn_open.classList.add('hidden');
+// click events functions
+const showColorsList = () => {
+  show_list_colors.classList.remove('show-icons-btn');
+  show_list_colors.classList.add('hidden');
 
   colorsData.forEach(color => {
     const inputContainer = document.createElement("div");
@@ -51,4 +55,37 @@ function showColorsList() {
   });
 }
 
-btn_open.addEventListener('click', showColorsList);
+const showFiltersList = () => {
+  if (filters_mobile) {
+    (filters_mobile as HTMLElement).style.display = 'inline-block';
+  }
+}
+
+const showOrderList = () => {
+  if (filter_order_mobile) {
+    (filter_order_mobile as HTMLElement).style.display = 'inline-block';
+  }
+}
+
+const closeOrderList = () => {
+  if (filter_order_mobile) {
+    (filter_order_mobile as HTMLElement).style.display = 'none';
+  }
+}
+
+const closeFiltersList = () => {
+  if (filters_mobile) {
+    (filters_mobile as HTMLElement).style.display = 'none';
+  }
+}
+
+// click events
+// colors list
+show_list_colors.addEventListener('click', showColorsList);
+show_list_colors.addEventListener('click', showColorsList);
+
+// filters btns mobile
+filters_btn.addEventListener('click', showFiltersList);
+order_btn.addEventListener('click', showOrderList);
+close_order_btn.addEventListener('click', closeOrderList);
+close_filters_btn.addEventListener('click', closeFiltersList);
