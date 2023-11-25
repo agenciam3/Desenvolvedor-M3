6995ea8f740b5fee001d1a64afb846e7e832552a
laynamoraes <laynamoraaes@hotmail.com>
2023-11-25 18:13:35 -0300

    add filtros de cor, tamanho e preço - desktop e função adicionar ao carrinho

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,14 +1,15 @@
 import { Product } from "./Product";
 
 let productsSection = document.getElementById("products");
-const products = document.querySelectorAll(".product");
+const products = document.getElementsByClassName("product");
 const loadMoreButton = document.getElementById("load-more");
+const colors = document.querySelectorAll(".color");
 const sizes = document.querySelectorAll(".size");
-let page = 1;
-let limit = 9;
+const prices = document.querySelectorAll(".price");
+let quantityProducts = document.querySelector("#quantity-products");
+let buyProductButton;
 let query = "";
 let sort = "";
-let extraQuery = "";
 
 function main() {
   getDataFromApi();
@@ -22,18 +23,19 @@ function createProduct(data: Product) {
   const productDescription = document.createElement("h3");
   const productPrice = document.createElement("p");
   const productInstallment = document.createElement("p");
-  const productButton = document.createElement("button");
+  buyProductButton = document.createElement("button");
 
   data.size.forEach((sizeClass) => {
     productContainer.classList.add(`size-${sizeClass}`);
   });
 
   productContainer.classList.add("product");
+  productContainer.classList.add(`color-${data.color.toLocaleLowerCase()}`);
   productImage.classList.add("product-image");
   productDescription.classList.add("product-description");
   productPrice.classList.add("product-price");
   productInstallment.classList.add("product-installment");
-  productButton.classList.add("product-button");
+  buyProductButton.classList.add("product-button");
 
   productImage.src = data.image;
   productDescription.innerHTML = data.name;
@@ -47,21 +49,19 @@ function createProduct(data: Product) {
     style: "currency",
     currency: "BRL",
   })}`;
-  productButton.innerHTML = "COMPRAR";
+  buyProductButton.innerHTML = "COMPRAR";
 
   productContainer.appendChild(productImage);
   productContainer.appendChild(productDescription);
   productContainer.appendChild(productPrice);
   productContainer.appendChild(productInstallment);
-  productContainer.appendChild(productButton);
+  productContainer.appendChild(buyProductButton);
 
   return productContainer;
 }
 
 function getDataFromApi() {
-  fetch(
-    `http://localhost:5000/products?_sort=${query}&_order=${sort}${extraQuery}`
-  )
+  fetch(`http://localhost:5000/products?_sort=${query}&_order=${sort}`)
     .then((data) => data.json())
     .then(function (data) {
       data.map((product: Product) => {
@@ -72,6 +72,11 @@ function getDataFromApi() {
 
         if (productId > 9) {
           productContainer.style.display = "none";
+        }
+
+        buyProductButton = document.getElementsByClassName("product-button");
+        for (var i = 0; i < buyProductButton.length; i++) {
+          buyProductButton[i].addEventListener("click", addShoppingCart);
         }
       });
     })
@@ -90,6 +95,79 @@ function loadMoreProducts() {
   });
 
   loadMoreButton.style.display = "none";
+}
+
+function filterColor() {
+  let countClick = 0;
+
+  colors.forEach((color) => {
+    color.addEventListener("click", function () {
+      // manipulação para ativar e desativar as outras opções
+      if (countClick == 0) {
+        color.classList.toggle("active");
+        colors.forEach((colorDisable: HTMLElement) => {
+          if (!colorDisable.classList.contains("active")) {
+            colorDisable.classList.add("off");
+          }
+        });
+        countClick++;
+        console.log("entrou no caso 0", countClick);
+      } else {
+        color.classList.toggle("active");
+        colors.forEach((colorDisable: HTMLElement) => {
+          if (colorDisable.classList.contains("off")) {
+            colorDisable.classList.remove("off");
+          }
+        });
+        console.log("entrou no caso 1", countClick);
+        countClick = 0;
+      }
+
+      // lógica para renderizar os produtos selecionados
+      const colorValueSelected = `color-${color.textContent
+        .trim()
+        .toLocaleLowerCase()}`;
+      const productsFilterColor = [];
+
+      loadMoreButton.style.display = "none";
+
+      for (let i = 0; i < products.length; i++) {
+        const product: any = products[i];
+        const classContain = product.classList.contains(colorValueSelected);
+
+        // acrescentando o produto no array de produtos filtrados
+        if (classContain) {
+          productsFilterColor.push(product);
+        }
+
+        console.log(productsFilterColor);
+
+        // condição para mostrar os produtos que contém a opção do filtro
+        if (
+          productsFilterColor.includes(product) &&
+          color.classList.contains("active")
+        ) {
+          product.style.display = "flex";
+        } else product.style.display = "none";
+
+        // caso nenhum filtro esteja ativo
+        if (!color.classList.contains("active")) {
+          loadMoreButton.style.display = "block";
+          productsSection.innerHTML = "";
+          return getDataFromApi();
+        }
+      }
+
+      // caso não haja nenhum produto com aquele tamanho
+      if (productsFilterColor.length == 0) {
+        console.log("não tem produto");
+        const withoutProduct = document.createElement("p");
+        withoutProduct.innerText = "Nenhum produto encontrado";
+
+        productsSection.appendChild(withoutProduct);
+      }
+    });
+  });
 }
 
 function addClassFilterSize() {
@@ -124,24 +202,23 @@ function filterSize() {
 
   sizes.forEach((size) => {
     size.addEventListener("click", function () {
-      const productsElement = document.getElementsByClassName("product");
       const sizeValueSelected = `size-${size.innerHTML}`;
-      const productsFilter = [];
+      const productsFilterSize = [];
 
       loadMoreButton.style.display = "none";
 
-      for (let i = 0; i < productsElement.length; i++) {
-        const product: any = productsElement[i];
+      for (let i = 0; i < products.length; i++) {
+        const product: any = products[i];
         const classContain = product.classList.contains(sizeValueSelected);
 
         // acrescentando o produto no array de produtos filtrados
         if (classContain) {
-          productsFilter.push(product);
+          productsFilterSize.push(product);
         }
 
-        // condição para mostrar ou não os elementos do filtro
+        // condição para mostrar os produtos que contém a opção do filtro
         if (
-          productsFilter.includes(product) &&
+          productsFilterSize.includes(product) &&
           size.classList.contains("active")
         ) {
           product.style.display = "flex";
@@ -156,7 +233,7 @@ function filterSize() {
       }
 
       // caso não haja nenhum produto com aquele tamanho
-      if (productsFilter.length == 0) {
+      if (productsFilterSize.length == 0) {
         console.log("não tem produto");
         const withoutProduct = document.createElement("p");
         withoutProduct.innerText = "Nenhum produto encontrado";
@@ -167,7 +244,89 @@ function filterSize() {
   });
 }
 
+function filterPrice() {
+  let countClick = 0;
+  let productsFilterPrice = [];
+
+  prices.forEach((price) => {
+    price.addEventListener("click", function () {
+      // manipulação para ativar e desativar as outras opções
+      // e detectar se é o primeiro ou segundo click naquele input
+      if (countClick == 0) {
+        price.classList.toggle("active");
+        prices.forEach((priceDisable: HTMLElement) => {
+          if (!priceDisable.classList.contains("active")) {
+            priceDisable.classList.add("off");
+          }
+        });
+        countClick++;
+        console.log("entrou no caso 0", countClick);
+      } else {
+        price.classList.toggle("active");
+        prices.forEach((priceDisable: HTMLElement) => {
+          if (priceDisable.classList.contains("off")) {
+            priceDisable.classList.remove("off");
+          }
+        });
+        console.log("entrou no caso 1", countClick);
+        countClick = 0;
+        productsFilterPrice = [];
+        loadMoreButton.style.display = "block";
+        productsSection.innerHTML = "";
+        return getDataFromApi();
+      }
+
+      const priceValueSelected = `color-${price.textContent
+        .trim()
+        .toLocaleLowerCase()}`;
+      const priceValueSplit = priceValueSelected.split("r$");
+      const priceOne = Number(priceValueSplit[1].split(" até")[0]);
+      const priceTwo = Number(priceValueSplit[2]);
+
+      loadMoreButton.style.display = "none";
+
+      for (let i = 0; i < products.length; i++) {
+        const product: any = products[i];
+
+        const productPrice = product.childNodes.item(2).textContent;
+        const productPriceNumber = Number(
+          productPrice.split("R$")[1].trim().replace(",", ".")
+        );
+
+        // produtos com dois valores de faixa de preço
+        if (productPriceNumber > priceOne && productPriceNumber <= priceTwo) {
+          product.style.display = "flex";
+          productsFilterPrice.push(product);
+          console.log("encontrei");
+
+          // produtos a partir de 500 reais
+        } else if (productPriceNumber > priceOne && Number.isNaN(priceTwo)) {
+          product.style.display = "flex";
+          productsFilterPrice.push(product);
+          console.log("enctrei de 500");
+
+          // produtos que não estão naquela faixa de preço
+        } else {
+          product.style.display = "none";
+          console.log("nenhum produto encontrado");
+        }
+      }
+
+      // caso não haja nenhum produto com aquele tamanho
+      if (productsFilterPrice.length == 0) {
+        console.log("não tem produto");
+        const withoutProduct = document.createElement("p");
+        withoutProduct.innerText = "Nenhum produto encontrado";
+
+        productsSection.appendChild(withoutProduct);
+      }
+    });
+  });
+}
+
+filterColor();
 filterSize();
+filterPrice();
 
 // função de selecionar uma opção do "Ordenar por:"
 let selectValue = document.getElementById("select-value"),
@@ -237,4 +396,10 @@ function seeAllColors() {
   });
 
   allColorsButton.style.display = "none";
+}
+
+function addShoppingCart() {
+  const addProduct = String(Number(quantityProducts.innerHTML) + 1);
+
+  quantityProducts.innerHTML = addProduct;
 }
